ce07c195a34fdfc834f9ffe33bc3544872017da6
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-15 14:56:22 -0600

    writing pseduo code for HW9 in index, adding dependencies

---
+++ Develop/index.js
@@ -0,0 +1,26 @@
+/* psedud code // require fs and inquirer,
+use an axios request to github,
+parse JSON data to become object 
+find a HTML to PDF converter library
+prompt the the user the background color of their choice
+prompt the user for their github user name
+write var for github url + prompt user name
+get dependencies inquirer, html+pdf converter, and fs
+
+
+ */
+//dependencies
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+    init();
